9008ff19319d281957bd8e1ee6852dc1c1a1c244
Roman Heinrich <roman.heinrich@gmail.com>
2008-09-02 19:40:52 +0200

    email added

--- name_generator.rb
+++ name_generator.rb
@@ -1,9 +1,13 @@
+# Taken from xl-suite (xlsuite.org) and slightly modified
+
 require "rubygems"
 require "active_record"
 require 'ostruct'
 last_names = File.open("dist.all.last.de", "rb") {|f| f.read}
 male_names = File.open("dist.male.first", "rb") {|f| f.read}
 female_names = File.open("dist.female.first", "rb") {|f| f.read}
+
+
 
 def extractor(list)
   list.scan(/^\w+/)
@@ -15,14 +19,15 @@ female_names = extractor(female_names).map(&:capitalize)
 middle_initials = ("A".."Z").to_a
 
 
-count = 100 
+count = 100 # how many names should be generated
 puts "Generating #{count} names"
-Name = Struct.new(:last, :first, :middle)
+Name = Struct.new(:last, :first, :middle, :main_email)
 Name.class_eval do 
- def to_s
-  "#{first} #{last}"
- end
+  def to_s
+    "#{first} #{last}, #{main_email}"
+  end
 end
+
 count.times do
   n = Name.new
   n.last = last_names[rand(last_names.size)]
@@ -33,11 +38,10 @@ count.times do
   end 
 
   n.middle = middle_initials[rand(26)] if rand() < 0.45
-  # Party.transaction do
-  #     party = account.parties.create!(:name => n)
-  #     party.main_email.update_attributes(:address => "#{n.first}.#{n.last}.#{party.id}@hotmail.com".downcase)
-  #     party.main_phone.update_attributes(:number => sprintf("%03d.%03d.%04d", rand(1000), rand(1000), rand(10000)))
-  #   end
+
+  # add other user-related fields and generate dummy-data for them
+  n.main_email =  "#{n.first}.#{n.last}@hotmail.com".downcase
+  # n.main_phone =  sprintf("%03d.%03d.%04d", rand(1000), rand(1000), rand(10000))
 
   puts n.to_s
 end
